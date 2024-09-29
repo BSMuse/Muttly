db32ba5ba7619ee07810c2ab1ea9ec944cf7aefe
Alexander Da Costa <bsmuse@tutamail.com>
2024-09-29 14:31:46 -0600

    Update breedUserLiked route to remove SQL injection error and clean up code

--- server/src/routes/breedsUserLiked.js
+++ server/src/routes/breedsUserLiked.js
@@ -2,49 +2,34 @@ require('dotenv').config();
 
 const express = require('express');
 const router = express.Router();
-const morgan = require('morgan');
-const path = require('path');
-const bodyParser = require('body-parser');
 
-const userLikedBreeds = require('../../database/queries/get_breeds_user_liked')
-const validateSession = require('../helpers/sessionValidation')
-const breedParentNames = require('../../database/queries/get_breed_name_extra_details')
+const userLikedBreeds = require('../../database/queries/get_breeds_user_liked');
+const validateSession = require('../helpers/sessionValidation');
+const breedParentNames = require('../../database/queries/get_breed_name_extra_details');
 
 router.get('/', validateSession, async (req, res) => {
+  try {
+    const userId = req.session.user.id;
 
-try {
+    if (!userId) {
+      return res.status(400).json({ error: 'Missing userId in the request' });
+    }
 
-  const userId = req.session.user.id;
+    const userLiked = await userLikedBreeds(userId);
+    const userLikedIds = userLiked.map((breed) => breed.id); // Convert to an array of IDs
 
-  // console.log("here is my userId attempt to validate:", userId)
+    // Handle case where userLikedIds is empty
+    if (userLikedIds.length === 0) {
+      return res.json({ userLiked, extraDetails: [] });
+    }
 
-  if (!userId) {
-    return res.status(400).json({ error: 'Missing dogBreedId in the request' });
+    const extraDetails = await breedParentNames(userLikedIds);
+
+    res.json({ userLiked, extraDetails });
+  } catch (error) {
+    console.error('Error executing SQL query:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  const userLiked = await userLikedBreeds(userId);
-
-  const userLikedIds = userLiked.map((breed) => ({ id: breed.id }));
-
-
-  // console.log("here is my check for breed ids: ", userLikedIds)
-
-  const extraDetails = await breedParentNames(userLikedIds)
-
-  // console.log('here is the extradetails back from the query', extraDetails)
-
-
-
-  // console.log('Fetched data dog details:', userLiked);
-
-
-  res.json({userLiked, extraDetails});
-
-} catch (error) {
-  console.error('Error executing SQL query:', error);
-  res.status(500).json({ error: 'Internal Server Error' });
-}
 });
-
 
 module.exports = router;
