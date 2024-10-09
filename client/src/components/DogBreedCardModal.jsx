729b52acf18cdcac087644715531b31b449ef3ed
Alexander Da Costa <bsmuse@tutamail.com>
2024-10-08 20:13:00 -0600

    Update DogBreedCardmodal to have better interface for mobile

--- client/src/components/DogBreedCardModal.jsx
+++ client/src/components/DogBreedCardModal.jsx
@@ -187,14 +187,16 @@ const DogBreedCardModal = (props) => {
             : '../icons/heart_empty.png'
           }></img></a>}
             </div>
-            {isValid && isMobile && <a><img className='modal-card-icons' onClick={onLikeClick} src={
-              liked ? '../icons/heart.png'
-            : '../icons/heart_empty.png'
-          }></img></a>}
-            {isValid ? <a><img className='modal-card-icons' onClick={onShareClick} src='../icons/share.png'></img></a>
-            : <a><img className='modal-card-icons'  onClick={onCloseClick} src='../icons/close.png'></img></a>}
-            {isValid && isMobile && (feed ? <a><img className='modal-card-icons'  onClick={onCloseClick} src='../icons/close.png'></img></a>
-            : <a><img className='modal-card-icons'  onClick={onTrashClick} src='../icons/trash-can.png'></img></a>)}
+            <div className='mobile-icons-ctn'>
+              {isValid && isMobile && <a><img className='modal-card-icons' onClick={onLikeClick} src={
+                liked ? '../icons/heart.png'
+              : '../icons/heart_empty.png'
+            }></img></a>}
+              {isValid ? <a><img className='modal-card-icons' onClick={onShareClick} src='../icons/share.png'></img></a>
+              : <a><img className='modal-card-icons'  onClick={onCloseClick} src='../icons/close.png'></img></a>}
+              {isValid && isMobile && (feed ? <a><img className='modal-card-icons'  onClick={onCloseClick} src='../icons/close.png'></img></a>
+              : <a><img className='modal-card-icons'  onClick={onTrashClick} src='../icons/trash-can.png'></img></a>)}
+            </div>
         </div>
       </div>}
     </>
