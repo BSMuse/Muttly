3efaeb3d9d4d77c918ba0df16651a1d1bf8ef356
Alexander Da Costa <bsmuse@tutamail.com>
2024-10-25 16:46:28 -0600

    Update sing in modal for easy login

--- client/src/components/SignInModal.jsx
+++ client/src/components/SignInModal.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
 import '../views/stylesheets/SignInModal.scss';
 
 const SignInModal = (props) => {
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { onClose } = props;
 
   const handleEmailChange = (e) => {
@@ -49,16 +48,22 @@ const SignInModal = (props) => {
     };
   };
 
+  const insertCredentials = () => {
+    setEmail('testing@email.com');
+    setPassword('demoPassword');
+    handleSignIn();
+  };
+
   return (
     <div className="signin-modal-overlay">
       <div className="signin-modal">
-        <span className="close-button" onClick ={(onClose)} >
+        <span className="close-button" onClick={onClose}>
           &times;
         </span>
         <div className="logo-header">
           <img src="../icons/paws_pink.png" alt="Muttly Logo" className="logo" />
           <h2>Sign In!</h2>
-          <h3>If you don't have an account, please click <a onClick ={(onClose)}><Link to="/signup">here</Link></a> to sign up!</h3>
+          <h3>If you don't have an account, please click <a onClick={onClose}><Link to="/signup">here</Link></a> to sign up!</h3>
         </div>
         <div className="form-container">
           <label htmlFor="email">Email Address</label>
@@ -78,8 +83,11 @@ const SignInModal = (props) => {
             value={password} 
             onChange={handlePasswordChange} 
           />
-          <br></br>
-          <button type="button" onClick={handleSignIn} >Sign In</button>
+          <br />
+          <div className="signin-btn-ctn">
+          <button type="button" onClick={handleSignIn}>Sign In</button>
+          <button type="button" onClick={insertCredentials}>Test Sign In</button>
+          </div>
         </div>
       </div>
     </div>
